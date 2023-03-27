7995a879eeeabbecc7b01bc8c9539e14ff9206f1
4elovel <marcynakvova007@gmail.com>
2023-03-27 18:16:08 +0300

    Добавьте файлы проекта.

---
+++ C#_start/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using static System.Console;
+class MyClass
+{
+    static void Main()
+    {
+        Int16 A = 1;
+        Int16 B = 5;
+        for (int i = A; i < B+1; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Write(i);
+            }
+            WriteLine();
+        }
+    }
+}
